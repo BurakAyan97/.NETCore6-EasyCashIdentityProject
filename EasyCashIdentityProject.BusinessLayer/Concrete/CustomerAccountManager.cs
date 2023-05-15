c08a6b809fb6f4dfa64a89cea0d0377a50d7e013
BurakAyan97 <burakayan97@outlook.com>
2023-05-15 22:26:11 +0300

    updated

---
+++ EasyCashIdentityProject.BusinessLayer/Concrete/CustomerAccountManager.cs
@@ -0,0 +1,46 @@
+﻿using EasyCashIdentityProject.BusinessLayer.Abstract;
+using EasyCashIdentityProject.DataAccessLayer.Abstract;
+using EasyCashIdentityProject.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCashIdentityProject.BusinessLayer.Concrete
+{
+    public class CustomerAccountManager : ICustomerAccountService
+    {
+        private readonly ICustomerAccountDal _customerAccountDal;
+
+        public CustomerAccountManager(ICustomerAccountDal customerAccountDal)
+        {
+            _customerAccountDal = customerAccountDal;
+        }
+
+        public void TDelete(CustomerAccount entity)
+        {
+           _customerAccountDal.Delete(entity);
+        }
+
+        public CustomerAccount TGetByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<CustomerAccount> TGetList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TInsert(CustomerAccount entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TUpdate(CustomerAccount entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
