2e73c7e422b90d4d4572cccd72cb63cddf3b97c5
BurakAyan97 <burakayan97@outlook.com>
2023-05-18 21:37:25 +0300

    updated

--- EasyCashIdentityProject.PresentationLayer/Controllers/RegisterController.cs
+++ EasyCashIdentityProject.PresentationLayer/Controllers/RegisterController.cs
@@ -1,8 +1,9 @@
 ﻿using EasyCashIdentityProject.DtoLayer.Dtos.AppUserDtos;
 using EasyCashIdentityProject.EntityLayer.Concrete;
+using MailKit.Net.Smtp;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-
+using MimeKit;
 namespace EasyCashIdentityProject.PresentationLayer.Controllers
 {
     public class RegisterController : Controller
@@ -25,20 +26,53 @@ namespace EasyCashIdentityProject.PresentationLayer.Controllers
         {
             if (ModelState.IsValid)
             {
+                Random random = new Random();
+                int code = random.Next(100000, 1000000);
+
                 AppUser appUser = new AppUser()
                 {
                     UserName = appUserRegisterDto.Username,
                     Name = appUserRegisterDto.Name,
                     Email = appUserRegisterDto.Email,
                     Surname = appUserRegisterDto.Surname,
+                    City = "İstanbul",
+                    District = "Maltepe",
+                    ImageUrl = "test",
+                    ConfirmCode = code,
                 };
                 var result = await _userManager.CreateAsync(appUser, appUserRegisterDto.Password);
 
                 if (result.Succeeded)
+                {
+                    MimeMessage mimeMessage = new MimeMessage();
+                    MailboxAddress mailboxAddressFrom = new MailboxAddress("Easy Cash Admin", "burakayan97@outlook.com");
+                    MailboxAddress mailboxAddressTo = new MailboxAddress("User", appUser.Email);
+
+                    mimeMessage.From.Add(mailboxAddressFrom);
+                    mimeMessage.To.Add(mailboxAddressTo);
+
+                    var bodyBuilder = new BodyBuilder();
+                    bodyBuilder.TextBody = "Kayıt işlemini gerçekleştirmek için onay kodunuz :" + code;
+                    mimeMessage.Body = bodyBuilder.ToMessageBody();
+
+                    mimeMessage.Subject = "Easy Cash Onay Kodu";
+
+                    SmtpClient smtpClient = new SmtpClient();
+                    smtpClient.Connect("smtp.gmail.com", 587, false);
+                    smtpClient.Authenticate("burakayan97@outlook.com", "");
+                    smtpClient.Send(mimeMessage);
+                    smtpClient.Disconnect(true);
+
+
+
                     return RedirectToAction("Index", "ConfirmMail");
+                }
                 else
+                {
                     foreach (var item in result.Errors)
                         ModelState.AddModelError("", item.Description);
+                }
+
             }
             return View();
         }
--- EasyCashIdentityProject.PresentationLayer/Program.cs
+++ EasyCashIdentityProject.PresentationLayer/Program.cs
@@ -1,5 +1,6 @@
 using EasyCashIdentityProject.DataAccessLayer.Concrete;
 using EasyCashIdentityProject.EntityLayer.Concrete;
+using EasyCashIdentityProject.PresentationLayer.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,7 +9,9 @@ builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<Context>();
 
-builder.Services.AddIdentity<AppUser, AppRole>().AddEntityFrameworkStores<Context>();
+builder.Services.AddIdentity<AppUser, AppRole>()
+                .AddEntityFrameworkStores<Context>()
+                .AddErrorDescriber<CustomIdentityValidator>();
 
 var app = builder.Build();
 
